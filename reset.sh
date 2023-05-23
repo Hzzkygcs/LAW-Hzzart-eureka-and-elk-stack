10b873ae1e80cab18715c1c99d595e20566a0e66
Immanuel01 <immanuel01@ui.ac.id>
2023-05-23 13:39:33 +0700

    chores: update reset.sh to not down the server to prevent database deletion

--- reset.sh
+++ reset.sh
@@ -1,4 +1,5 @@
-sudo docker compose down || true
+sudo docker compose stop || true
+#sudo docker compose down || true
 git pull origin main
 sudo rm -d -r ./consul-data
 sudo docker compose up --build -d
